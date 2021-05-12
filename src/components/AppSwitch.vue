464b05cb9cfb17d2f8a4663ed133962ac5bdcc1f
JIAJUNATBCIT <49431626+JIAJUNATBCIT@users.noreply.github.com>
2021-05-11 20:49:35 -0700

    init

---
+++ src/components/AppSwitch.vue
@@ -0,0 +1,14 @@
+<template>
+    <button @click="$emit('toggle-add-task')" :style="{background: color}" class='btn'>{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'AppSwitch',
+    props: {
+        text: String,
+        color: String
+    }
+}
+</script>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <AppSwitch v-show="homePage"
+        @toggle-add-task="$emit('toggle-add-task')" 
+        :text="showAddTask ? 'Close': 'Add Task'" 
+        :color="showAddTask ? 'red' : 'green'" />
+    </header>
+</template>
+
+<script>
+import AppSwitch from './AppSwitch'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        AppSwitch
+    },
+    computed: {
+        homePage() {
+            return this.$route.path === '/'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
